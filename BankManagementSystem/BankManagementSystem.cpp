176b60a2eadb45f614503f5b7f66ccd0677ca41c
igor6629 <nechepurenko6629@gmail.com>
2022-12-29 16:21:51 +0300

    Adding Client class to BankManagementSystem

--- BankManagementSystem/BankManagementSystem.cpp
+++ BankManagementSystem/BankManagementSystem.cpp
@@ -1,8 +1,13 @@
 ﻿#include <iostream>
+#include "Client.h"
 
 using namespace std;
+using namespace ClientSpace;
 
 int main()
 {
+	Client client = Client("John", "Collins");
+	client.ShowInfo();
+	
 	return 0;
 }
