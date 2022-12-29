90c1ccad058bfdb85d1865cccc96fb890c36f826
igor6629 <nechepurenko6629@gmail.com>
2022-12-29 23:14:24 +0300

    Continue creating menu

--- BankManagementSystem/BankManagementSystem.cpp
+++ BankManagementSystem/BankManagementSystem.cpp
@@ -2,15 +2,49 @@
 #include "Client.h"
 
 using namespace std;
-using namespace ClientSpace;
+using namespace Account;
+
+void header()
+{
+	cout << "-----------------------------------------------------------------" << endl;
+	cout << "|\t\t\tBANK OF AMERICA \t\t\t|" << endl;
+	cout << "-----------------------------------------------------------------" << endl << endl;
+}
+
+void menu()
+{
+	cout << "\tMENU" << endl;
+	cout << "___________________" << endl;
+	cout << "1) Create Account\n" << "2) Sign In\n" << "3) Exit" << endl << endl;
+}
 
 int main()
 {
-	cout << "----------------------------------------------------------------------------" << endl;
-	cout << "BANK OF AMERICA" << endl;
-	cout << "----------------------------------------------------------------------------" << endl << endl;
-	cout << "MENU" << endl;
-	cout << "1) Create Account\n" << "2) Sign In\n";
+	header();
+	menu();
+	
+	int operation = 0;
 
+	do
+	{
+		cout << "Select an operation: ";
+		cin >> operation;
+	} while (operation > 3 || operation <= 0);
+
+	switch (operation)
+	{
+	case 1:
+		header();
+		cout << "Hi" << endl;
+		break;
+	case 2:
+		header();
+		cout << "Sign in" << endl;
+	case 3:
+		return 0;
+	default:
+		break;
+	}
+	
 	return 0;
 }
