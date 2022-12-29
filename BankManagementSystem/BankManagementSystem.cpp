580241aa704d696f06bb19fc2ca528e49b5072d9
igor6629 <nechepurenko6629@gmail.com>
2022-12-29 19:06:53 +0300

    Delete test Client

--- BankManagementSystem/BankManagementSystem.cpp
+++ BankManagementSystem/BankManagementSystem.cpp
@@ -5,13 +5,6 @@ using namespace std;
 using namespace ClientSpace;
 
 int main()
-{
-	Client client = Client("John", "Collins");
-	client.ShowInfo();
-
-	client.Deposit(500);
-	client.Withdraw(400);
-	client.Withdraw(500);
-	
+{	
 	return 0;
 }
