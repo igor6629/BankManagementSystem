40d23ad2a1a75b6aa9b7b51dcbeeb89dd888f556
igor6629 <nechepurenko6629@gmail.com>
2022-12-29 21:42:55 +0300

    Create menu

--- BankManagementSystem/BankManagementSystem.cpp
+++ BankManagementSystem/BankManagementSystem.cpp
@@ -5,6 +5,12 @@ using namespace std;
 using namespace ClientSpace;
 
 int main()
-{	
+{
+	cout << "----------------------------------------------------------------------------" << endl;
+	cout << "BANK OF AMERICA" << endl;
+	cout << "----------------------------------------------------------------------------" << endl << endl;
+	cout << "MENU" << endl;
+	cout << "1) Create Account\n" << "2) Sign In\n";
+
 	return 0;
 }
